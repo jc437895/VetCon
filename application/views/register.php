fdd244129f102d3ebcd64ed620494b6d03752c73
JC437895 (Frank YIN) <45928772+jc437895@users.noreply.github.com>
2019-05-15 09:15:58 +1000

    Create register.php
    Create register page with a register form

---
+++ application/views/register.php
@@ -0,0 +1,22 @@
+<div class="register-page">
+  <div class="form">
+    <h4>Register form</h4>
+    <h6>Please fill up this form for registration</h6>
+    <form class="form-register">
+      <input class="form-group" id="username" type="text" placeholder="username(*)"/>
+      <div class="validation"></div>
+      <input class="form-group" id="password" type="password" placeholder="password(*)"/>
+      <div class="validation"></div>
+      <input class="form-group" id="firstName" type="text" placeholder="First name"/>
+      <input class="form-group" id="lastName" type="text" placeholder="Last name"/>
+      <input class="form-group" id="phoneNumber" type="number" placeholder="Phone number"/>
+      <input class="form-group" id="email" type="email" placeholder="Email"/>
+      <input class="form-group" id="position" type="text" placeholder="position"/>
+      <textarea class="form-control" id="jobDuties" name="Job duties" rows="5" placeholder="Job duties (within 200 words)"></textarea>
+      <button class="btn-register">Register</button>
+    </form>
+    <?php?>
+    <a href="login.php">Already registered? Login </a>
+    <?php?>
+  </div>
+</div>
