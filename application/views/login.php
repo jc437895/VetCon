21fa2c39e7a9aa2c5c0b95692e1cbda7fb13f334
JC437895 (Frank YIN) <45928772+jc437895@users.noreply.github.com>
2019-05-15 09:11:55 +1000

    Create login.php
    login page with a login form

---
+++ application/views/login.php
@@ -0,0 +1,15 @@
+<div class="login-page">
+  <div class="form">
+    <h4>Login form</h4>
+    <form class="form">
+      <input class="form-group" type="text" placeholder="username"/>
+      <div class="validation"></div>
+      <input class="form-group" type="password" placeholder="password"/>
+      <div class="validation"></div>
+      <button class="btn-login">login</button>
+    </form>
+    <?php?>
+    <a href="register.php">New user? Register an account</a>
+    <?php?>
+  </div>
+</div>
