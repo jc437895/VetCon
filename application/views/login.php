bcef187feff0ef1080d11a11e4ad901692818deb
JC437895 (Frank YIN) <45928772+jc437895@users.noreply.github.com>
2019-05-15 12:16:45 +1000

    Login and Registration added

---
+++ application/views/login.php
@@ -0,0 +1,14 @@
+
+<div class="login-page">
+  <div class="form">
+    <h4>Login form</h4>
+    <form class="form">
+      <input class="form-group" type="text" placeholder="username"/>
+      <div class="validation"></div>
+      <input class="form-group" type="password" placeholder="password"/>
+      <div class="validation"></div>
+      <button class="btn-login">login</button>
+    </form>
+    <a href="register.html">New user? Register an account</a>
+  </div>
+</div>
